7e85a74e4b0360cb83fd93518b56d9f05fb648c9
andy <andysalerno@gmail.com>
2021-03-14 15:02:05 -0700

    Have a client/server, but about to try switching to websockes

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,17 @@
+use salt_engine::id::Id;
 use server::messages::NewGameResponse;
-use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://localhost:3030/newgame").await?;
-    println!("{:?}", resp);
+    let game_id = join_game().await?;
 
-    let resp = resp.json::<NewGameResponse>().await?;
-    println!("{:#?}", resp);
     Ok(())
+}
+
+async fn join_game() -> Result<Id, Box<dyn std::error::Error>> {
+    let resp = reqwest::get("http://localhost:3030/newgame").await?;
+    let resp = resp.json::<NewGameResponse>().await?;
+
+    println!("Joining game: {:#?}", resp.game_id);
+    Ok(resp.game_id)
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,11 @@
 mod matchmaking_queue;
 pub mod messages;
 
-use models::{new_queue, PlayerQueue};
+use models::{new_queue, GameQueue};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
     let goodbye = warp::path!("goodbye" / String).map(|name| format!("goodbye, {}!", name));
 
@@ -18,7 +17,7 @@ async fn main() {
 }
 
 pub fn new_game(
-    queue: PlayerQueue,
+    queue: GameQueue,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("newgame")
         .and(warp::get())
@@ -26,39 +25,67 @@ pub fn new_game(
         .and_then(handlers::new_game)
 }
 
+pub fn game(
+    queue: GameQueue,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("game" / String)
+        .and(warp::get())
+        .and(with_queue(queue))
+        .and_then(handlers::game)
+}
+
 fn with_queue(
-    queue: PlayerQueue,
-) -> impl Filter<Extract = (PlayerQueue,), Error = std::convert::Infallible> + Clone {
+    queue: GameQueue,
+) -> impl Filter<Extract = (GameQueue,), Error = std::convert::Infallible> + Clone {
     warp::any().map(move || queue.clone())
 }
 
 mod handlers {
-    use crate::{messages::NewGameResponse, models::PlayerQueue};
-    use salt_engine::{game_state::PlayerId, id::Id};
+    use crate::{messages::NewGameResponse, models::GameQueue};
+    use salt_engine::id::Id;
     use std::convert::Infallible;
 
-    pub async fn new_game(queue: PlayerQueue) -> Result<impl warp::Reply, Infallible> {
-        // First, try to pop from the queue if possible.
+    pub async fn new_game(queue: GameQueue) -> Result<impl warp::Reply, Infallible> {
         let mut queue = queue.lock().await;
-        queue.push_front(PlayerId::new());
+        if let Some(game_id) = queue.pop_back() {
+            println!("Placing player in existing game: {:?}", game_id);
+            let r = NewGameResponse::new(game_id);
+            Ok(warp::reply::json(&r))
+        } else {
+            let game_id = Id::new();
+            println!("Placing player in new game: {:?}", game_id);
+            queue.push_front(game_id);
+            let r = NewGameResponse::new(game_id);
+            Ok(warp::reply::json(&r))
+        }
+    }
 
-        let mut r = NewGameResponse::new(Id::new());
-        r.players_in_queue = queue.len();
-
-        Ok(warp::reply::json(&r))
+    pub async fn game(id: String, queue: GameQueue) -> Result<impl warp::Reply, Infallible> {
+        let mut queue = queue.lock().await;
+        if let Some(game_id) = queue.pop_back() {
+            println!("Placing player in existing game: {:?}", game_id);
+            let r = NewGameResponse::new(game_id);
+            Ok(warp::reply::json(&r))
+        } else {
+            let game_id = Id::new();
+            println!("Placing player in new game: {:?}", game_id);
+            queue.push_front(game_id);
+            let r = NewGameResponse::new(game_id);
+            Ok(warp::reply::json(&r))
+        }
     }
 }
 
 mod models {
-    use salt_engine::game_state::PlayerId;
+    use salt_engine::id::Id;
     use std::{collections::VecDeque, sync::Arc};
     use tokio::sync::Mutex;
 
     /// So we don't have to tackle how different database work, we'll just use
     /// a simple in-memory DB, a vector synchronized by a mutex.
-    pub type PlayerQueue = Arc<Mutex<VecDeque<PlayerId>>>;
+    pub type GameQueue = Arc<Mutex<VecDeque<Id>>>;
 
-    pub fn new_queue() -> PlayerQueue {
+    pub fn new_queue() -> GameQueue {
         Arc::new(Mutex::new(VecDeque::new()))
     }
 }
