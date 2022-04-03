46b21ff93f1be2ccb7d05d0fab2e8909790a56f7
andy salerno <andysalerno@gmail.com>
2022-04-03 16:53:53 -0700

    Fix the different borrow levels and add tests

--- game_state/src/entity.rs
+++ game_state/src/entity.rs
@@ -1,3 +1,5 @@
+use std::borrow::{Borrow, BorrowMut};
+
 use crate::id::Id;
 use serde::{de::DeserializeOwned, Serialize};
 
@@ -19,35 +21,61 @@ impl Entity {
         }
     }
 
-    pub fn typed<T: IsEntity>(self) -> TypedEntity<T> {
-        TypedEntity::<T> {
+    pub fn into_typed<T: IsEntity>(self) -> TypedEntity<T, serde_json::Value> {
+        TypedEntity {
             id: self.id,
             data: self.data,
             _phantom: std::marker::PhantomData::default(),
         }
     }
+
+    pub fn as_typed_mut<T: IsEntity>(&mut self) -> TypedEntity<T, &mut serde_json::Value> {
+        TypedEntity {
+            id: self.id,
+            data: &mut self.data,
+            _phantom: std::marker::PhantomData::default(),
+        }
+    }
+
+    pub fn as_typed<T: IsEntity>(&self) -> TypedEntity<T, &serde_json::Value> {
+        TypedEntity {
+            id: self.id,
+            data: &self.data,
+            _phantom: std::marker::PhantomData::default(),
+        }
+    }
 }
 
-pub struct TypedEntity<T: IsEntity> {
+pub struct TypedEntity<T: IsEntity, I> {
     id: Id,
-    data: serde_json::Value,
+    data: I,
     _phantom: std::marker::PhantomData<T>,
 }
 
-impl<T: IsEntity> TypedEntity<T> {
+impl<T, I> TypedEntity<T, I>
+where
+    T: IsEntity,
+    I: Borrow<serde_json::Value>,
+{
     pub fn get<F, R>(&self, f: F) -> R
     where
         F: FnOnce(&T) -> R,
     {
-        let local: T = serde_json::from_value(self.data.clone()).unwrap();
+        let local: T = serde_json::from_value(self.data.borrow().clone()).unwrap();
         f(&local)
     }
+}
 
+impl<T, I> TypedEntity<T, I>
+where
+    T: IsEntity,
+    I: BorrowMut<serde_json::Value>,
+{
     pub fn get_mut(&mut self, f: impl FnOnce(&mut T)) {
-        let mut local: T = serde_json::from_value(self.data.clone()).unwrap();
+        let mut local: T = serde_json::from_value(self.data.borrow().clone()).unwrap();
         f(&mut local);
 
-        self.data = serde_json::to_value(local).unwrap();
+        *self.data.borrow_mut() = serde_json::to_value(local).unwrap();
     }
 }
 
@@ -89,7 +117,7 @@ mod tests {
 
         let entity = Entity::new(test_entity);
 
-        let read = entity.typed::<TestEntity>().get(|e| e.i);
+        let read = entity.into_typed::<TestEntity>().get(|e| e.i);
 
         assert_eq!(99, read);
     }
@@ -103,7 +131,40 @@ mod tests {
 
         let entity = Entity::new(test_entity);
 
-        let mut typed_entity = entity.typed::<TestEntity>();
+        let mut typed_entity = entity.into_typed::<TestEntity>();
+
+        typed_entity.get_mut(|e| e.i = 100);
+        let read = typed_entity.get(|e| e.i);
+
+        assert_eq!(100, read);
+    }
+
+    #[test]
+    fn test_entity_as_typed_can_be_read() {
+        let test_entity = TestEntity {
+            i: 99,
+            ..Default::default()
+        };
+
+        let entity = Entity::new(test_entity);
+
+        let typed_entity = entity.as_typed::<TestEntity>();
+
+        let read = typed_entity.get(|e| e.i);
+
+        assert_eq!(99, read);
+    }
+
+    #[test]
+    fn test_entity_as_typed_mut_can_be_updated() {
+        let test_entity = TestEntity {
+            i: 99,
+            ..Default::default()
+        };
+
+        let mut entity = Entity::new(test_entity);
+
+        let mut typed_entity = entity.as_typed_mut::<TestEntity>();
 
         typed_entity.get_mut(|e| e.i = 100);
         let read = typed_entity.get(|e| e.i);
