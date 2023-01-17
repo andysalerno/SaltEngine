ab91e8a93e66e4e9de7c5a2165a9d390fca61aeb
andy <andy@andys-MBP.CaveOnePrimary>
2023-01-16 21:51:27 -0800

    hacky way of filling the hand

--- webclient/src/js.js
+++ webclient/src/js.js
@@ -1,3 +1,11 @@
+// The global context.
+const context = {
+    socket: null,
+    myId: null,
+    enemyId: null,
+    myHand: []
+};
+
 function addCardToSlot() {
     const template = document.getElementById("card-board-template");
     const cloned = template.content.cloneNode(true);
@@ -8,9 +16,8 @@ function addCardToSlot() {
 }
 
 function removeCardFromSlot() {
-    const slot = document.querySelectorAll(".board-row.my-side-2 > .card-slot")[2];
-    const child = slot.querySelector(".card-board");
-
+    // const slot = document.querySelectorAll(".board-row.my-side-2 > .card-slot")[2];
+    // const child = slot.querySelector(".card-board");
     // slot.removeChild(child);
 }
 
@@ -19,14 +26,19 @@ function wsConnect() {
 
     const textBox = document.querySelector(".extra-zone");
 
-    socket.onopen = (event) => {
+    socket.onopen = () => {
         textBox.innerHTML += "Opened!</br>";
     };
 
     socket.onmessage = (message) => {
         textBox.innerHTML += message.data + "</br>";
+
+        const parsed = JSON.parse(message.data);
+
+        onMessageReceived(parsed);
     };
 
+    context.socket = socket;
 }
 
 function setUpEvents() {
@@ -40,6 +52,39 @@ function setUpEvents() {
         }
 
     });
+}
+
+function logGameMessage(message) {
+    const rightBox = document.querySelectorAll(".extra-zone")[1];
+
+    rightBox.innerHTML += message + "</br>";
+}
+
+function onMessageReceived(message) {
+    if (message.Hello) {
+        context.myId = message.Hello[0].guid;
+        context.enemyId = message.Hello[1].guid;
+    } else if (message.Event.kind === "CardDrawnClientEvent") {
+        const body = JSON.parse(message.Event.body);
+        handleCardDrawn(body);
+    }
+}
+
+function handleCardDrawn(event) {
+    if (event.player_id.guid == context.myId) {
+        logGameMessage("I drew a card.");
+        const cardsCount = context.myHand.push(event.card_drawn.Visible);
+        const index = cardsCount - 1;
+
+        const template = document.getElementById("card-hand-template");
+        const cloned = template.content.cloneNode(true);
+
+        const myHand = document.querySelector(".my-hand");
+        const handSlot = myHand.querySelectorAll(".hand-slot")[index];
+        handSlot.appendChild(cloned);
+    } else {
+        logGameMessage("Enemy drew a card.");
+    }
 }
 
 addCardToSlot();
