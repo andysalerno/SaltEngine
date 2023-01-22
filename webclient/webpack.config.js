10cf784d25dd0745d9b3e1e02b7d7f64a60f11f7
Andy Salerno <andysalerno@gmail.com>
2023-01-21 18:16:44 -0800

    A working webpack situation.

--- webclient/webpack.config.js
+++ webclient/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/js.ts',
-    devTool: 'inline-source-map',
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
